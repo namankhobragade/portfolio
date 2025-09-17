c05500c5bfbb37611813a44c391759437c6ec92c
Sunil khobragade (Naman) <naman.intelcode@gmail.com>
2025-09-17 08:25:37 +0000

    no I want little more give both side more space (_for element div_)

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -6,7 +6,7 @@ import { AnimatedItem } from "../animated-item";
 export function Experience() {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32">
-      <div className="container px-4 md:px-6">
+      <div className="container px-4 md:px-12 lg:px-24">
         <div className="space-y-8">
             <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">Work Experience</h2>
