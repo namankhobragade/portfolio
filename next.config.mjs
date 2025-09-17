31bda007a6cab92656935e5d6ce8fae3f02e9808
Sunil khobragade (Naman) <naman.intelcode@gmail.com>
2025-09-17 13:46:16 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // This is to ensure that server-side dependencies are correctly bundled.
-    if (isServer) {
-      config.externalsPresets = { ...config.externalsPresets, node: true };
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack(config) {
+    config.externals.push({
+      '@google-cloud/vertexai-preview':
+        'commonjs @google-cloud/vertexai-preview',
+      '@google-cloud/vertexai': 'commonjs @google-cloud/vertexai',
+    });
     return config;
   },
 };
